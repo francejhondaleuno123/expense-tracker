5f2a86a51f68fa69925cab3d5016b97481c22934
France Jhondale Uno <fjbuno@mymail.mapua.edu.ph>
2026-09-21 14:36:10 +0800

    Add Express server with test route

---
+++ expense-tracker/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'API is working!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
